38588302cbe8d093ad09f2450794f606f8a4301f
Cimawills <61263735+Cimawills@users.noreply.github.com>
2026-04-29 08:04:34 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import { Analytics } from "@vercel/analytics/next"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Portfolio';
